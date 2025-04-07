33cb65fecd95a6ab4fa626ddd9229e04f9a5b35c
QuocToan1604 <huynhquoctoan1642002@gmail.com>
2025-04-07 22:33:47 +0700

    video 33

--- BackEnd0/src/config/database.js
+++ BackEnd0/src/config/database.js
@@ -1,13 +1,25 @@
 require('dotenv').config()
 const mysql = require('mysql2')
 
+//cach cũ
+//const connection = mysql.createconnection({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT || 3306, //default:3306
+//     user: process.env.DB_USER, // default: empty
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+// });
+
 // Create the connection to database
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306, //default:3306
     user: process.env.DB_USER, // default: empty
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = connection;
