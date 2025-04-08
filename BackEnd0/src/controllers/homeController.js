db8bc10c09abde1266e0d9d05f7b48d734729500
QuocToan1604 <huynhquoctoan1642002@gmail.com>
2025-04-08 11:57:15 +0700

    video 35

--- BackEnd0/src/controllers/homeController.js
+++ BackEnd0/src/controllers/homeController.js
@@ -2,17 +2,7 @@ const connection = require('../config/database')
 
 
 const getHomepage = (req, res) => {
-    //process data
-    //call model
-    connection.query(
-        'select * from Users u ',
-        function (err, results, fields) {
-            users = results;
-            console.log(">>>>results=", results); // results contains rows returned by server
-            // console.log(">>>>check user", users);
-            res.send(JSON.stringify(users));
-        }
-    );
+    return res.render('home.ejs')
 }
 
 const getABC = (req, res) => {
