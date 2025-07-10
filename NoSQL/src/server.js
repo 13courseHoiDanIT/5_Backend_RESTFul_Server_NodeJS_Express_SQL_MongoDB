39ef92beb85bc82a2a9d8611d953a4dbb58d84cf
QuocToan1604 <huynhquoctoan1642002@gmail.com>
2025-07-11 00:51:04 +0700

    video 202

--- NoSQL/src/server.js
+++ NoSQL/src/server.js
@@ -9,8 +9,13 @@ const hostname = process.env.HOST_NAME
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+
+//config static file
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! check check')
 })
 
 app.get('/abc', (req, res) => {
