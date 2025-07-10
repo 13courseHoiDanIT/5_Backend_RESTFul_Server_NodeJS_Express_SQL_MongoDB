315c7f15594a4bd1ed207e127d1765094e9c2432
QuocToan1604 <huynhquoctoan1642002@gmail.com>
2025-07-11 02:04:46 +0700

    video 212

--- NoSQL/src/server.js
+++ NoSQL/src/server.js
@@ -4,6 +4,7 @@ const path = require('path')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
 
+const mysql = require('mysql2')
 
 const app = express()
 const port = process.env.PORT || 8888;
@@ -17,6 +18,22 @@ configViewEngine(app);
 //khai baos route
 app.use('/', webRoutes)
 
+//test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'hoidanit',
+    port: 3307,
+    password: '123456'
+});
+
+connection.query(
+    'SELECT * FROM Users u',
+    function (err, results, fields) {
+        console.log("resukts>>", results); // results contains rows returned by server
+        console.log("=", fields); // fields contains extra meta data about results, if available
+    }
+);
 
 
 app.listen(port, hostname => {
