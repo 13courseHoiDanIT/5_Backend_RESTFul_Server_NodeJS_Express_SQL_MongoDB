50d1c905e867300d3c86fd5e184f3a90ebf9b6c8
QuocToan1604 <huynhquoctoan1642002@gmail.com>
2025-07-11 11:53:30 +0700

    video 217

--- NoSQL/src/controllers/homeController.js
+++ NoSQL/src/controllers/homeController.js
@@ -3,18 +3,7 @@ const connection = require('../config/database')
 
 
 const getHomePage = (req, res) => {
-    //process data
-    //call model
-    let users = [];
-    connection.query(
-        'SELECT * FROM Users u',
-        function (err, results, fields) {
-            users = results;
-            console.log("resukts>>", results); // results contains rows returned by server
-            console.log("check users", users)
-            res.send(JSON.stringify(users))
-        }
-    );
+    return res.render('home.ejs')
 
 }
 
