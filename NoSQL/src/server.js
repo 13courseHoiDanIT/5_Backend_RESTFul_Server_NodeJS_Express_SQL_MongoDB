c046f0667fe54644cc91971bbc2b83cfc9b46c2f
QuocToan1604 <huynhquoctoan1642002@gmail.com>
2025-07-12 18:37:58 +0700

    video 261

--- NoSQL/src/server.js
+++ NoSQL/src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const path = require('path')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const apiRoutes = require('./routes/api')
 const connection = require('./config/database')
 const mongoose = require('mongoose');
 
@@ -21,6 +22,7 @@ configViewEngine(app);
 
 //khai baos route
 app.use('/', webRoutes);
+app.use('/v1/api', apiRoutes);
 
 //test connection
 
